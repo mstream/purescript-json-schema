49ee412a79b0543375d1cf58208fdc624b304d8d
Maciej Laciak <maciej.laciak@gmail.com>
2023-11-19 12:30:39 +0000

    docs: add a provisional JSON validation sandbox (#6)

--- docs.sh
+++ docs.sh
@@ -3,4 +3,6 @@
 set -e
 
 spago run --main Docs.Main
+spago bundle-app --main Sandbox.Main --to docs/src/sandbox.js
+echo '<html><head><title>Sandbox</title></head><body><script src="./sandbox.js"></script></body></html>' >docs/src/sandbox.html
 nix build .#docs
--- src/purs/Main/CLI.purs
+++ src/purs/Main/CLI.purs
@@ -2,38 +2,22 @@ module Main.CLI (main) where
 
 import Prelude
 
-import Control.Monad.Error.Class (throwError)
-import Data.Argonaut.Core as A
-import Data.Argonaut.Encode as AE
-import Data.Argonaut.Parser as AP
 import Data.Either (Either(..))
 import Data.Foldable (fold)
-import Data.Markdown as M
-import Data.Newtype (wrap)
-import Docs.Document (document)
 import Effect (Effect)
-import Effect.Aff (Aff, error, launchAff_)
+import Effect.Aff (Aff, launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Class.Console as Console
-import JsonSchema.Codec.Parsing as Parsing
-import JsonSchema.Validation as Validation
-import JsonValue (JsonValue(..))
+import Main.Validate as Validate
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff as FS
+import Node.Process as Process
 import Options.Applicative (Parser, ParserInfo, ReadM, (<**>))
 import Options.Applicative as O
 
 type Options = { command ∷ Command }
 
-data Command = Validate ValidateOptions
-
-type ValidateOptions =
-  { jsonPath ∷ String
-  , outputFormat ∷ OutputFormat
-  , schemaPath ∷ String
-  }
-
-data OutputFormat = Json | Markdown
+data Command = Validate Validate.Options
 
 optionsParser ∷ Parser Options
 optionsParser = ado
@@ -47,12 +31,12 @@ commandParser = O.subparser
           (O.progDesc "Validate a JSON value against a schema")
       )
 
-parseOutputFormat ∷ ReadM OutputFormat
+parseOutputFormat ∷ ReadM Validate.OutputFormat
 parseOutputFormat = O.eitherReader case _ of
   "json" →
-    Right Json
+    Right Validate.Json
   "markdown" →
-    Right Markdown
+    Right Validate.Markdown
   unsupportedFormat →
     Left $ "Unsupported format '" <> unsupportedFormat <> "'"
 
@@ -63,7 +47,7 @@ validateParser = ado
   jsonPath ← O.strOption $ fold
     [ O.long "json", O.metavar "FILE" ]
   outputFormat ← O.option parseOutputFormat $ fold
-    [ O.long "output-format", O.short 'o', O.value Json ]
+    [ O.long "output-format", O.short 'o', O.value Validate.Json ]
   in Validate { jsonPath, outputFormat, schemaPath }
 
 main ∷ Effect Unit
@@ -82,40 +66,12 @@ run { command } = case command of
       <> " against schema at "
       <> schemaPath
 
-    jsonText ← FS.readTextFile UTF8 jsonPath
-
-    jsonValue ← case AP.jsonParser jsonText of
-      Left errorMessage →
-        throwError
-          $ error
-          $ "Failed to parse the JSON value document: " <> errorMessage
-      Right json →
-        pure json
-
     schemaText ← FS.readTextFile UTF8 schemaPath
-
-    schema ←
-      case
-        Parsing.parseSchema =<< (wrap <$> AP.jsonParser schemaText)
-        of
-        Left errorMessage →
-          throwError
-            $ error
-            $ "Failed to parse the JSON schema document: " <>
-                errorMessage
-        Right json →
-          pure json
-
+    jsonText ← FS.readTextFile UTF8 jsonPath
     let
-      output = case outputFormat of
-        Json →
-          A.stringifyWithIndent 2 $ AE.encodeJson $ JsonValue jsonValue
-            `Validation.validateAgainst` schema
-        Markdown →
-          M.render { maxLineLength: 72 }
-            $ M.document
-            $ document
-            $ JsonValue jsonValue `Validation.validateAgainst` schema
+      { exitCode, stderr, stdout } = Validate.compute
+        { jsonText, outputFormat, schemaText }
 
-    Console.info $ output <> "\n"
-    pure unit
+    Console.info stdout
+    Console.error stderr
+    liftEffect $ Process.exit' exitCode
