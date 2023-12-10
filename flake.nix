60b21ba0d5c196508d6f4784974adcc80912f47d
mstream <maciej.laciak@gmail.com>
2023-12-10 21:51:10 +0000

    chore: update nixpkgs version

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     easy-purescript-nix.url = "github:justinwoo/easy-purescript-nix/master";
     flake-utils.url = "github:numtide/flake-utils/main";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   };
 
   outputs =
@@ -135,7 +135,6 @@
           devShellInputs.node-packages ++
           devShellInputs.pkgs;
         inputsFrom = [ docs ];
-        PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
         shellHook = ''
           PS1="\[\e[33m\][\[\e[m\]\[\e[34;40m\]${name}:\[\e[m\]\[\e[36m\]\w\[\e[m\]\[\e[33m\]]\[\e[m\]\[\e[32m\]\\$\[\e[m\] "
         '';
